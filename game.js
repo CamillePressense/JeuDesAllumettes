32541f18bc5e3be6d8f74c9f61e2e8ff96df25bf
Pressense Fasani <camillefasani@gmail.com>
2025-03-06 09:52:11 +0100

    Etape 2 : prompt + add index.html

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 let totalOfMatches = 50;
+let nbToRemove;
 
 /* Étape 1
 Faire une fonction qui prend en paramètre le nombre d'allumettes à enlever du reste.
@@ -7,3 +8,13 @@ Pour rappel, un nom de fonction doit décrire clairement ce que fait celle-ci. *
 function removeMatches(nbToRemove){
     totalOfMatches -= nbToRemove;
 }
+
+/* Étape 2
+Demander a l'utilisateur combien d'allumettes il souhaite retirer tant 
+qu’il y a des allumettes dans le tas. Pour rappel, on a 50 allumettes au départ. */
+
+while (totalOfMatches > 0){
+    nbToRemove = prompt("Combien d'allumettes souhaites tu retirer ?");
+}
+
+console.log(nbToRemove)
