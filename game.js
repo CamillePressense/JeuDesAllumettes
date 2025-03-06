ad95cc5436b3af67dd849c1a42471de5d4ff075b
Pressense Fasani <camillefasani@gmail.com>
2025-03-06 15:24:32 +0100

    Etape 3 : Refacto

--- game.js
+++ game.js
@@ -2,9 +2,9 @@ const TOTALOFMATCHES = 50;
 
 // Tant qu'il y a des allumettes, je demande à l'utilisateur un nombre entre 1 et 6 et je renvoie cette valeur.
 
-function askNbToRemove(nb){
+function askNbToRemove(nbOfMatches){
     let userNbToRemove;
-    if(nb > 0){
+    if(nbOfMatches > 0){
         userNbToRemove = Number(prompt("Combien d'allumettes souhaites tu retirer? Choisis un nombre entre 1 et 6."));
         while (!(userNbToRemove > 0 && userNbToRemove <= 6)){
             userNbToRemove = prompt("On a dit entre 1 et 6 ! Combien d'allumettes souhaites tu retirer ?")
@@ -14,14 +14,10 @@ function askNbToRemove(nb){
 }
 // Je déduis du nombre d'allumettes restantes la quantité choisie par l'utilisateur et renvoie le total restant.
 
-function removeMatches(total, nb){
-    console.log( "le nb est :" + nb)
-    console.log("le total entré dans la fonction est " + total)
+function removeMatches(totalOfMatches, nbOfMatches){
     let totalRemaining;
-    total -= nb;
-    console.log("le total après soustraction est" + total)
-    totalRemaining = total;
-    console.log("le totalRemaining est " + totalRemaining)
+    totalOfMatches -= nbOfMatches;
+    totalRemaining = totalOfMatches;
     console.log(`Il reste ${totalRemaining} allumettes.`)
     return totalRemaining;
 }
@@ -29,16 +25,12 @@ function removeMatches(total, nb){
 // Je définis le déroulement du jeu, et la victoire quand la dernière allumette a été prise.
 
 function gamePlay(){
-    let userNb;
-    userNb = askNbToRemove(TOTALOFMATCHES);
-
-    let remainder;
-    remainder = removeMatches(TOTALOFMATCHES,userNb);
-    console.log(remainder);
-
+    let userNbOfMatches;
+    let remainder = TOTALOFMATCHES;
+    
     while (remainder > 0){
-        userNb = askNbToRemove(remainder);
-        remainder = removeMatches(remainder, userNb);
+        userNbOfMatches = askNbToRemove(remainder);
+        remainder = removeMatches(remainder, userNbOfMatches);
         if (remainder <= 0){
             alert("Bravo, tu as récupéré la dernière allumette. Tu as gagné!")
         }
