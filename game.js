750d5e30b7d02112e2f4aad3989d275b6a57a455
Pressense Fasani <camillefasani@gmail.com>
2025-03-06 09:44:03 +0100

    Etape 1 : create removeMatches function

---
+++ game.js
@@ -0,0 +1,9 @@
+let totalOfMatches = 50;
+
+/* Étape 1
+Faire une fonction qui prend en paramètre le nombre d'allumettes à enlever du reste.
+Pour rappel, un nom de fonction doit décrire clairement ce que fait celle-ci. */
+
+function removeMatches(nbToRemove){
+    totalOfMatches -= nbToRemove;
+}
