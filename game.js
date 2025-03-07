8e72279f4dd0f5278ab41b6dc3a7528325ed9c4b
Pressense Fasani <camillefasani@gmail.com>
2025-03-07 10:33:51 +0100

    Etape 4 : two players game

--- game.js
+++ game.js
@@ -2,10 +2,10 @@ const TOTALOFMATCHES = 50;
 
 // Tant qu'il y a des allumettes, je demande à l'utilisateur un nombre entre 1 et 6 et je renvoie cette valeur.
 
-function askNbToRemove(nbOfMatches){
+function askNbToRemove(nbOfMatches, player){
     let userNbToRemove;
     if(nbOfMatches > 0){
-        userNbToRemove = Number(prompt("Combien d'allumettes souhaites tu retirer? Choisis un nombre entre 1 et 6."));
+        userNbToRemove = Number(prompt(`${player} Combien d'allumettes souhaites tu retirer? Choisis un nombre entre 1 et 6.`));
         while (!(userNbToRemove > 0 && userNbToRemove <= 6)){
             userNbToRemove = prompt("On a dit entre 1 et 6 ! Combien d'allumettes souhaites tu retirer ?")
         }
@@ -22,17 +22,25 @@ function removeMatches(totalOfMatches, nbOfMatches){
     return totalRemaining;
 }
 
-// Je définis le déroulement du jeu, et la victoire quand la dernière allumette a été prise.
+// Je définis le déroulement du jeu à deux joueurs, et la victoire quand la dernière allumette a été prise.
 
 function gamePlay(){
-    let userNbOfMatches;
+    let player1NbOfMatches;
+    let player2NbOfMatches;
     let remainder = TOTALOFMATCHES;
     
     while (remainder > 0){
-        userNbOfMatches = askNbToRemove(remainder);
-        remainder = removeMatches(remainder, userNbOfMatches);
+        player1NbOfMatches = askNbToRemove(remainder, "Joueur 1");
+        remainder = removeMatches(remainder, player1NbOfMatches);
+
         if (remainder <= 0){
-            alert("Bravo, tu as récupéré la dernière allumette. Tu as gagné!")
+            alert("Bravo Joueur 1, tu as récupéré la dernière allumette. Tu as gagné!")
+        }
+        player2NbOfMatches = askNbToRemove(remainder, "Joueur 2");
+        remainder = removeMatches(remainder, player2NbOfMatches);
+
+        if (remainder <= 0){
+            alert("Bravo Joueur 2, tu as récupéré la dernière allumette. Tu as gagné!")
         }
     }    
 }
